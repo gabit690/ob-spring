a775b89f8cf81fc94f4b901fa8feabce7d6ec882
gabit <gabit690@gmail.com>
2022-07-19 16:42:16 -0300

    Initial Commit

---
+++ src/main/java/com/gabit/oppractice/OpPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.gabit.oppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabit/oppractice/OpPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabit.oppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
